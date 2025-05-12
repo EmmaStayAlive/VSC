5441edf1faf504fcf17684ce7110afdf5518f5c1
Emma <emma0370k@gmail.com>
2025-05-12 13:52:20 -0600

    Cuarto cambio

---
+++ Snippets.py
@@ -0,0 +1,17 @@
+# Un snippet es un fragmento de código que se puede reutilizar en diferentes partes de un programa.
+# En este caso, vamos a crear un snippet para imprimir una presentación en Python.
+
+print('Hola mi nombre es Emmanuel y tengo 17 años. Me gusta Programar.')
+print('Hola mi nombre es Alicia y tengo 13 años. Me gusta Dibujar.')
+
+
+#
+# Snippet para crear un programa que imprima una presentacion{
+#	"Presentacion":{
+#		"prefix": "Presentacion", "body":
+#		[
+#			"print('Hola mi nombre es ${1:tu nombre} y tengo ${2:tu edad} años. Me gusta ${3:tu pasatiempo favorito}.')"
+#		],
+#		"description": "Esta es una practica para crear un snippet en python"
+#	}
+#}
